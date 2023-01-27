770600d38a8863d6f1c9961840ee06f554b024b8
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 23:27:44 -0400

    Create LogicWeek1.cs

---
+++ LogicWeek1.cs
@@ -0,0 +1,24 @@
+/*1. Muestre la secuencia de Fibonacci hasta la cantidad de elementos indicados indicada por el usuario por el teclado.*/
+
+using System;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 1, c = 1;
+            int fin, cont = 1;
+            Console.Write("Digitar la cantidad deseada en la serie fibonacci: ");
+            fin = Convert.ToInt32(Console.ReadLine());
+            for (cont = 1; cont <= fin; cont++)
+            {
+                Console.WriteLine($"Secuencia {cont}: {a}");
+                a = b;
+                b = c;
+                c = a + b;
+            }
+        }
+    }
+}
