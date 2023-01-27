85e72747db64a3c1b3aecff7e0c1d6a15f815f29
Ismael Martínez <121949680+ISMARZDEV@users.noreply.github.com>
2023-01-26 23:28:19 -0400

    Create LogicWeek.cs

---
+++ LogicWeek.cs
@@ -0,0 +1,25 @@
+/*2. Pida un número y determine si es par o impar*/
+
+using System;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num;
+            Console.Write("Digite un número: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"El numero {num} es par");
+            }
+            else
+            {
+                Console.WriteLine($"El numero {num} es impar");
+            }
+        }
+    }
+}
